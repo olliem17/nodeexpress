b1c49ca017443046072ca92e0f3d1f4ae4ee37a4
olliem17 <ogmuir@gmail.com>
2021-04-30 16:05:01 +1200

    added bootstrap styling

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/user-list', function(req, res, next) {
     var sql='SELECT * FROM users';
     db.query(sql, function (err, data, fields) {
     if (err) throw err;
-    res.render('user-list', { title: 'User List', userData: data});
+    res.render('user-list', { title: 'Users', userData: data});
   });
 });
 
